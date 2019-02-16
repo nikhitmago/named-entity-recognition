4223cf3001d023092721dd79ff8afd75318f4356
Nikhit Mago <36119526+nikhitmago@users.noreply.github.com>
2019-02-15 16:31:45 -0800

    deleted comments

--- generate_features.py
+++ generate_features.py
@@ -8,39 +8,12 @@ for filename in filenames:
     globals()[filename[:-4]] = OrderedDict()
 
 def preprocess_corpus(train_sents):
-    """Use the sentences to do whatever preprocessing you think is suitable,
-    such as counts, keeping track of rare features/words to remove, matches to lexicons,
-    loading files, and so on. Avoid doing any of this in token2features, since
-    that will be called on every token of every sentence.
-
-    Of course, this is an optional function.
-
-    Note that you can also call token2features here to aggregate feature counts, etc.
-    """
     global filename
     for filename in filenames:
         globals()[filename[:-4]] = np.load(filename).item()
     
 
 def token2features(sent, i, add_neighs = True):
-    """Compute the features of a token.
-
-    All the features are boolean, i.e. they appear or they do not. For the token,
-    you have to return a set of strings that represent the features that *fire*
-    for the token. See the code below.
-
-    The token is at position i, and the rest of the sentence is provided as well.
-    Try to make this efficient, since it is called on every token.
-
-    One thing to note is that it is only called once per token, i.e. we do not call
-    this function in the inner loops of training. So if your training is slow, it's
-    not because of how long it's taking to run this code. That said, if your number
-    of features is quite large, that will cause slowdowns for sure.
-
-    add_neighs is a parameter that allows us to use this function itself in order to
-    recursively add the same features, as computed for the neighbors. Of course, we do
-    not want to recurse on the neighbors again, and then it is set to False (see code).
-    """
     ftrs = []
     # bias
     ftrs.append("BIAS")
